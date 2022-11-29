f5270bd1f396eb92e967460369771ea794bf06db
Brandon Nesterenko <bnesterenko09@gmail.com>
2022-11-29 23:18:04 +0000

    Initial commit of run script

---
+++ Makefile
@@ -0,0 +1,47 @@
+BENCHMARK_SCRIPT=runner.sh
+
+
+MARIADB_VERSION := $(shell $(MARIADB_HOME)/bin/mariadb --version | grep -Po "\K10.\d+[^,]+")
+
+run_benchmark: export BVAR_INNODB_FLUSH_LOG=$(INNODB_FLUSH_LOG)
+run_benchmark: export BVAR_SYNC_BINLOG=$(SYNC_BINLOG)
+run_benchmark: export BVAR_WAIT_COUNT=$(WAIT_COUNT)
+run_benchmark: export BVAR_WAIT_USEC=$(WAIT_USEC)
+run_benchmark: export BVAR_N_QUERIES=$(N_QUERIES)
+run_benchmark: export BVAR_RUN_ID=$(RUN_ID)
+run_benchmark: export BVAR_MARIADB_VERSION=$(MARIADB_VERSION)
+
+run_benchmark:
+	@echo {run_id:$(RUN_ID), innodb_flush:$(INNODB_FLUSH_LOG), sync_binlog:$(SYNC_BINLOG), wait_count:$(WAIT_COUNT), wait_usec:$(WAIT_USEC), n_queries:$(N_QUERIES), version:"$(MARIADB_VERSION)"}
+	./$(BENCHMARK_SCRIPT) $(N_CONNECTIONS)
+
+run_n_connections:
+	@$(MAKE) N_CONNECTIONS=16
+	@$(MAKE) N_CONNECTIONS=32
+	@$(MAKE) N_CONNECTIONS=64
+
+run_sync_opts:
+	@$(MAKE) INNODB_FLUSH_LOG=1 SYNC_BINLOG=0 run_n_connections
+	@$(MAKE) INNODB_FLUSH_LOG=0 SYNC_BINLOG=1 run_n_connections
+	@$(MAKE) INNODB_FLUSH_LOG=1 SYNC_BINLOG=1 run_n_connections
+
+run_wait_counts:
+	@$(MAKE) WAIT_COUNT=2 run_sync_opts
+	@$(MAKE) WAIT_COUNT=4 run_sync_opts
+	@$(MAKE) WAIT_COUNT=8 run_sync_opts
+	@$(MAKE) WAIT_COUNT=16 run_sync_opts
+	@$(MAKE) WAIT_COUNT=32 run_sync_opts
+
+run_wait_usecs:
+	@$(MAKE) WAIT_USEC=100 run_wait_counts
+	@$(MAKE) WAIT_USEC=1000 run_wait_counts
+	@$(MAKE) WAIT_USEC=10000 run_wait_counts
+	@$(MAKE) WAIT_USEC=100000 run_wait_counts
+
+run_nqueries:
+	@$(MAKE) N_QUERIES=1000 run_wait_usecs
+
+run_benchmark_full:
+	@$(MAKE) RUN_ID=1 run_nqueries
+	@$(MAKE) RUN_ID=2 run_nqueries
+
---
+++ runner.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+  echo "Incorrect args"
+  echo "Call with args ./runner.sh <connection_count>"
+  exit 1
+fi
+
+connection_count=$1
+
+if [[ -z "${MARIADB_HOME}" ]];
+then
+  echo "Environment variable MARIADB_HOME must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_MARIADB_VERSION}" ]];
+then
+  echo "Environment variable BVAR_MARIADB_VERSION must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_INNODB_FLUSH_LOG}" ]];
+then
+  echo "Environment variable BVAR_INNODB_FLUSH_LOG must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_SYNC_BINLOG}" ]];
+then
+  echo "Environment variable BVAR_SYNC_BINLOG must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_WAIT_COUNT}" ]];
+then
+  echo "Environment variable BVAR_WAIT_COUNT must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_WAIT_USEC}" ]];
+then
+  echo "Environment variable BVAR_WAIT_USEC must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_N_QUERIES}" ]];
+then
+  echo "Environment variable BVAR_N_QUERIES must be set"
+  exit 1
+fi
+
+if [[ -z "${BVAR_RUN_ID}" ]];
+then
+  echo "Environment variable BVAR_RUN_ID must be set"
+  exit 1
+fi
+
+DATABASE="bench"
+RESULT_DIR=results
+RUN_DIR=~/run/mariadb
+DATA_DIR=$RUN_DIR/data
+
+CFG_FILE=benchmark.cnf
+
+AGGREGATE_RESULT_FILE="flush_benchmark.csv"
+if [ ! -f "$AGGREGATE_RESULT_FILE" ]; then
+  echo "version,run_id,connection_count,connection_no,innodb_flush_log_at_trx_commit,sync_binlog,binlog_commit_wait_count,binlog_commit_wait_usec,n_queries,engine,benchmark_type,average_time_to_run_queries,min_time_to_run_queries,max_time_to_run_queries,clients_running_queries,queries_per_client" > $AGGREGATE_RESULT_FILE
+fi
+
+rm -rf $RESULT_DIR
+mkdir -p $RESULT_DIR
+
+start_server() {
+  echo "Starting Server.."
+  $MARIADB_HOME/bin/mysqld --defaults-file=$CFG_FILE
+  echo "Server successfully shutdown"
+  exit 0
+}
+
+do_update() {
+  table_id=$1
+  result_file="$RESULT_DIR/out.t${table_id}.csv"
+  echo "Slapping table ${DATABASE}.t$table_id"
+  $MARIADB_HOME/bin/mysqlslap --defaults-file=$CFG_FILE --query="update ${DATABASE}.t$table_id set b=b+1 where a=1" --engine="innodb" --csv="$result_file" --delimiter=";" --concurrency=1 --number-of-queries=$BVAR_N_QUERIES --create-schema="$DATABASE"
+
+  while IFS="," read -r engine mode avg min max nclients queries_per_client
+  do
+    echo "$BVAR_MARIADB_VERSION,$BVAR_RUN_ID,$connection_count,$1,$BVAR_INNODB_FLUSH_LOG,$BVAR_SYNC_BINLOG,$BVAR_WAIT_COUNT,$BVAR_WAIT_USEC,$BVAR_N_QUERIES,$engine,update,$avg,$min,$max,$nclients,$queries_per_client" >> $AGGREGATE_RESULT_FILE
+  done < $result_file
+
+}
+
+cp template.cnf $CFG_FILE
+
+echo "datadir=$DATA_DIR" >> $CFG_FILE
+echo "innodb_flush_log_at_trx_commit=$BVAR_INNODB_FLUSH_LOG" >> $CFG_FILE
+echo "sync_binlog=$BVAR_SYNC_BINLOG" >> $CFG_FILE
+echo "binlog_commit_wait_count=$BVAR_WAIT_COUNT" >> $CFG_FILE
+echo "binlog_commit_wait_usec=$BVAR_WAIT_USEC" >> $CFG_FILE
+
+echo "Initializing mariadbd.."
+rm -rf $RUN_DIR
+mkdir -p $RUN_DIR
+$MARIADB_HOME/scripts/mysql_install_db --basedir=$MARIADB_HOME --datadir=$DATA_DIR --defaults-file=$CFG_FILE
+
+echo "Starting mariadbd.."
+start_server &
+SERVER_PID=$!
+sleep 1
+echo "..Started"
+
+echo "Creating tables.."
+$MARIADB_HOME/bin/mariadb --socket=/tmp/mysql.sock -e "CREATE DATABASE IF NOT EXISTS $DATABASE"
+for (( c=1; c<=$1; c++ ))
+do 
+  $MARIADB_HOME/bin/mariadb --socket=/tmp/mysql.sock -e "CREATE TABLE ${DATABASE}.t$c (a int, b int) engine=innodb;insert into ${DATABASE}.t$c (a, b) values (1, 1);"
+done
+
+for (( c=1; c<=$1; c++ ))
+do 
+  do_update $c &
+done
+
+
+err=0
+
+for job in `jobs -p`
+do
+  if [ $job -ne $SERVER_PID ];
+  then
+    wait $job || let "err+=1"
+  fi
+done
+
+echo "Updates complete, shutting down server"
+$MARIADB_HOME/bin/mariadb --socket=/tmp/mysql.sock -e "SHUTDOWN"
+
+wait $SERVER_PID
+
+if [ "$err" -ne "0" ];
+then
+  echo "$err Mariadb connections failed"
+fi
+
+echo ""
